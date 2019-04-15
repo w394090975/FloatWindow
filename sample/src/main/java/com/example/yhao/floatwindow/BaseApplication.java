733c06b675defd2b70a5e853a50446b0bd129efa
wangandi <wangandi@gridsum.com>
2019-04-15 16:14:36 +0800

    Add virtual joystick floatview.

--- sample/src/main/java/com/example/yhao/floatwindow/BaseApplication.java
+++ sample/src/main/java/com/example/yhao/floatwindow/BaseApplication.java
@@ -1,12 +1,12 @@
 package com.example.yhao.floatwindow;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
-import android.os.Build;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.yhao.fixedfloatwindow.R;
 import com.yhao.floatwindow.FloatWindow;
@@ -14,6 +14,8 @@ import com.yhao.floatwindow.MoveType;
 import com.yhao.floatwindow.PermissionListener;
 import com.yhao.floatwindow.Screen;
 import com.yhao.floatwindow.ViewStateListener;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 /**
  * Created by yhao on 2017/12/18.
@@ -29,31 +31,41 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setImageResource(R.drawable.icon);
+        View view = LayoutInflater.from(this).inflate(R.layout.activity_floatview,null);
+        final TextView mTextViewAngleRight = view.findViewById(R.id.textView_angle_right);
+        final TextView mTextViewStrengthRight = view.findViewById(R.id.textView_strength_right);
+        final TextView mTextViewCoordinateRight = view.findViewById(R.id.textView_coordinate_right);
+        final JoystickView joystickRight = view.findViewById(R.id.joystickView_right);
+        joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleRight.setText(angle + "°");
+                mTextViewStrengthRight.setText(strength + "%");
+                mTextViewCoordinateRight.setText(
+                        String.format("x%03d:y%03d",
+                                joystickRight.getNormalizedX(),
+                                joystickRight.getNormalizedY())
+                );
+            }
+        });
 
         FloatWindow
                 .with(getApplicationContext())
-                .setView(imageView)
-                .setWidth(Screen.width, 0.2f) //设置悬浮控件宽高
-                .setHeight(Screen.width, 0.2f)
-                .setX(Screen.width, 0.8f)
+                .setView(view)
+                .setWidth(Screen.width, 0.5f) //设置悬浮控件宽高
+                .setHeight(Screen.width, 0.5f)
+                .setX(Screen.width, 0.4f)
                 .setY(Screen.height, 0.3f)
-                .setMoveType(MoveType.slide,100,-100)
+//                .setMoveType(MoveType.slide, -100, -100)
+                .setMoveType(MoveType.active)
                 .setMoveStyle(500, new BounceInterpolator())
-                .setFilter(true, A_Activity.class, C_Activity.class)
+//                .setFilter(true, A_Activity.class, C_Activity.class)
+                .setFilter(false)
                 .setViewStateListener(mViewStateListener)
                 .setPermissionListener(mPermissionListener)
                 .setDesktopShow(true)
                 .build();
-
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(BaseApplication.this, "onClick", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     private PermissionListener mPermissionListener = new PermissionListener() {
